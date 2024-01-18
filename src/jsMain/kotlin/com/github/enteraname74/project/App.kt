4dd7472f9e94f5aad8ed6196384ca586e927c29f
Noah PENIN <noahpenin@gmail.com>
2024-01-18 11:30:14 +0100

    Initial commit

---
+++ src/jsMain/kotlin/com/github/enteraname74/project/App.kt
@@ -0,0 +1,31 @@
+package com.github.enteraname74.project
+
+import io.kvision.Application
+import io.kvision.CoreModule
+import io.kvision.BootstrapModule
+import io.kvision.BootstrapCssModule
+import io.kvision.MapsModule
+import io.kvision.html.div
+import io.kvision.module
+import io.kvision.panel.root
+import io.kvision.startApplication
+
+class App : Application() {
+    override fun start() {
+        root("kvapp") {
+            div("Hello world")
+            // TODO
+        }
+    }
+}
+
+fun main() {
+    startApplication(
+        ::App,
+        module.hot,
+        BootstrapModule,
+        BootstrapCssModule,
+        MapsModule,
+        CoreModule
+    )
+}
